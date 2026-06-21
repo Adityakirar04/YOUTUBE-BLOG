9413cf05942934649d7833884812fa09b69018ec
Adityakirar04 <mr.kirar62@gmail.com>
2026-06-21 21:22:47 +0530

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,63 @@
+ const { createHmac, randomBytes } = require("crypto");
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    salt: {
+      type: String,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profileImageURL: {
+      type: String,
+      default: "/images/default.webp",
+    },
+
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  const salt = randomBytes(16).toString();
+
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  user.salt = salt;
+  user.password = hashedPassword;
+
+  next();
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
